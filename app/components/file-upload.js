85eed5298c91ae879bf671c2164782f6d914bcce
Albert Pak <al@albert-pak.com>
2018-04-25 01:55:56 -0500

    wip

--- app/components/file-upload.js
+++ app/components/file-upload.js
@@ -1,16 +1,39 @@
 import TextField from "@ember/component/text-field";
+import PapaParse from "papaparse";
 
 export default TextField.extend({
   type: "file",
   attributeBindings: ["name"],
-  csvData: null,
-  csvFile: null,
+  csvFileData: null,
 
-  change(evt) {
-    const input = evt.target;
+  sendParsedData(csvFileData) {
+    console.log("sendParsedData::this", this);
+
+    this.attrs.csvFilexUploaded(csvFileData);
+  },
+
+  parseData(file, callback) {
+    console.log("parseData::this", this);
+
+    PapaParse.parse(file, {
+      download: true,
+      dynamicTyping: true,
+      header: true,
+      skipEmptyLines: true,
+      trimHeader: true,
+
+      complete: function(results) {
+        callback(results.data);
+      }
+    });
+  },
+
+  change(e) {
+    const input = e.target;
 
     if (input.files && input.files[0]) {
-      this.attrs.csvFileUploaded(input.files[0]);
+      console.log("change::this", this);
+      this.parseData(input.files[0], this.sendParsedData);
     }
 
     return false;
--- app/controllers/bonds.js
+++ app/controllers/bonds.js
@@ -1,30 +1,11 @@
 import Controller from "@ember/controller";
-import { set } from "@ember/object";
-import PapaParse from "papaparse";
 
 export default Controller.extend({
   csvFileData: null,
 
-  csvFileUploaded(uploadedFile) {
-    // this.set("uploadedFile", uploadedFile);
+  csvFileUploaded(csvFileData) {
+    console.log("outside:csvFileData", csvFileData);
 
-    let uploadedCsvData;
-
-    PapaParse.parse(uploadedFile, {
-      download: true,
-      dynamicTyping: true,
-      header: true,
-      skipEmptyLines: true,
-      trimHeader: true,
-
-      complete(results) {
-        console.log("results", results);
-        uploadedCsvData = results.data;
-      }
-    });
-
-    console.log("uploadedCsvData", uploadedCsvData);
-
-    this.set("csvFileData", uploadedCsvData);
+    this.set("csvFileData", csvFileData);
   }
 });
