1883667c4c2f3ab6d1edb90f788facbbadce48a5
Albert Pak <al@albert-pak.com>
2018-04-26 18:12:56 -0500

    grid setup

--- app/components/investment-table.js
+++ app/components/investment-table.js
@@ -1,5 +1,11 @@
 import Component from '@ember/component';
 
 export default Component.extend({
-  tagName: 'table'
+  tagName: 'table',
+  classNames: [
+    'table',
+    'table-dark',
+    'table-striped',
+    'table-condensed'
+  ]
 });
